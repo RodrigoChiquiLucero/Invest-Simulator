ae0196b4126a1eddcba419fcb8a8a986b31aba32
rusito23 <i.andruskiewitsch23@gmail.com>
2018-10-09 19:54:34 -0300

    lista de activos con mock - falta manejar errores

--- Game/models.py
+++ Game/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+import Game.AssetInterfaceControl.interface_control as interface_control
 
-# Create your models here.
+
+class Asset(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=75)
+
+    def get_info(self):
+        # TODO: pedir informacion a la api
+        return interface_control.get_asset_by_id(id=self.id)
+
+
+class Wallet(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    assets = models.ManyToManyField(Asset)
+    liquid = models.FloatField(null=False)
+
+
+class Transaction(models.Model):
+    id = models.IntegerField(primary_key=True)
+    wallet_id = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
+    asset_price = models.FloatField(null=False)
+    date = models.DateField()
+    quantity = models.IntegerField()
+    is_purchase = models.BooleanField(null=False)
