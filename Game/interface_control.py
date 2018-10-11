a612ed03344458ca3979b83fe2dd5383c3821a8f
rusito23 <i.andruskiewitsch23@gmail.com>
2018-10-11 17:36:26 -0300

    agregando testCases y modificando el mock, ahora es propio de la api y sirve para hacer los test

--- Game/interface_control.py
+++ Game/interface_control.py
@@ -2,9 +2,9 @@ import urllib3.request
 import json
 from Game.models import AssetStruct
 
-API_URL = "https://bbe401af-1ad7-417c-b32b-a823a6d34b03.mock.pstmn.io/"
+API_URL = "http://localhost:8000/tests/"
 GET_ASSETS = "getAvailableAssets/"
-GET_QUOTE = "getAssetMarketPrice/?name="
+GET_QUOTE = "getAssetMarketPrice/"
 
 
 def url_to_json(url):
@@ -14,28 +14,41 @@ def url_to_json(url):
     if res.status == 200:
         return json.loads(res.data.decode())
     else:
+        # notify error
         return 0
 
 
 def get_asset_names():
     url = API_URL + GET_ASSETS
     json_assets = url_to_json(url)
-    if json_assets != 0:
+    asset_list = []
+    try:
+        if json_assets != 0:
+            json_assets = json_assets['availableAssets']
+            for a in json_assets:
+                asset = AssetStruct(name=a['name'], asset_type=a['type'])
+                asset_list.append(asset)
+            return asset_list
+    except KeyError:
+        # rollback
         asset_list = []
-        for a in json_assets:
-            asset = AssetStruct(name=a['name'], asset_type=a['type'])
-            asset_list.append(asset)
+    finally:
         return asset_list
-    return []
 
 
 def get_asset_quote(asset):
     url = API_URL + GET_QUOTE + asset.name
     asset_quote = url_to_json(url)
-    if asset_quote != 0:
-        asset.buy = asset_quote['buy']
-        asset.sell = asset_quote['sell']
-    return asset
+    try:
+        if asset_quote != 0:
+            asset.buy = asset_quote['buy']
+            asset.sell = asset_quote['sell']
+    except KeyError:
+        # rollback
+        asset.buy = -1
+        asset.sell = -1
+    finally:
+        return asset
 
 
 def has_quote(asset):
--- Game/tests.py
+++ Game/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from Game import interface_control as ic
+from Game.models import AssetStruct
 
-# Create your tests here.
+
+class InterfaceControlTest(TestCase):
+
+    def setUp(self):
+        return
+        # ic.API_URL = 'http://localhost:8000/tests/'
+
+    def test_sanity(self):
+        # search for the names
+        assets_name = ic.get_asset_names()
+        self.assertNotEqual(assets_name, [])
+
+        # search for all info
+        asset_info = [ic.get_asset_quote(a) for a in assets_name if ic.has_quote(ic.get_asset_quote(a))]
+        self.assertNotEqual(asset_info, [])
+
+        # dedicated full method
+        assets_complete = ic.get_assets()
+        self.assertNotEqual(assets_complete, [])
+
+    def test_border(self):
+        # ask for a quote of unexistent asset
+        asset = AssetStruct(name="NONE", asset_type="NONE")
+        quote = ic.get_asset_quote(asset)
+        self.assertEqual(quote, asset)
+        self.assertEqual(quote.buy, -1)
+        self.assertEqual(quote.sell, -1)
+
+        # ask for a quote of existent asset
+        asset = AssetStruct(name="MARSHALL", asset_type="currency")
+        quote = ic.get_asset_quote(asset)
+        self.assertNotEqual(asset, quote)
+        self.assertNotEqual(quote.buy, -1)
+        self.assertNotEqual(quote.sell, -1)
+
