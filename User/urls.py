3cb748bea8c87f702c24a6a9d63a01d2135339c6
FAguGomez <fagugomez1997@gmail.com>
2018-10-12 21:03:30 -0300

    Tabs changed into buttons

--- User/urls.py
+++ User/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.view_profile, name='view_profile'),
-    path('change_password/', views.change_password, name='change_password'),
+    path('profile/password/', views.change_password, name='password'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
 ]
