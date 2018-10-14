06e85be9c592c8993ecd1a6c2990f05aa43d6106
marcocba <markitocab@gmail.com>
2018-10-14 17:49:14 -0300

    Agrego campos, verificaciones de email, modifico el front, usando las herramientas de django en vez de manual

--- User/form.py
+++ User/form.py
@@ -4,15 +4,41 @@ from django.contrib.auth.models import User
 
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    email1 = forms.EmailField(required=True)
+    email2 = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("first_name",
+                  "last_name",
+                  "username",
+                  "email1",
+                  "email2",
+                  "password1",
+                  "password2")
 
     def save(self, commit=True):
         user = super(RegistrationForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.email = self.cleaned_data["email1"]
         if commit:
             user.save()
         return user
+
+    def clean_email2(self):
+        email1 = self.cleaned_data.get("email1")
+        email2 = self.cleaned_data.get("email2")
+        if email1 and email2 and email1 != email2:
+            raise forms.ValidationError(
+                'Emails do not match'
+            )
+        else:
+            inuse = not User.objects.filter(email=self.cleaned_data["email2"])
+            if not inuse:
+                raise forms.ValidationError(
+                    'Email is already in use'
+                )
+        return email2
