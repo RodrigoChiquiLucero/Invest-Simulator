4cbbe5f0456d5610de8e127ca5b3898afbc8bc37
marcocba <markitocab@gmail.com>
2018-10-14 18:02:33 -0300

    Verifico integridad de nombres

--- User/form.py
+++ User/form.py
@@ -28,17 +28,41 @@ class RegistrationForm(UserCreationForm):
             user.save()
         return user
 
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get("first_name")
+        if not (all(x.isalpha() or x.isspace() for x in first_name)):
+            raise forms.ValidationError(
+                'Enter alphabetical characters and spaces only.'
+            )
+        if len(first_name) > 20:
+            raise forms.ValidationError(
+                'Name must contain 20 characters or less.'
+            )
+        return first_name
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get("last_name")
+        if not (all(x.isalpha() or x.isspace() for x in last_name)):
+            raise forms.ValidationError(
+                'Enter alphabetical characters and spaces only.'
+            )
+        if len(last_name) > 20:
+            raise forms.ValidationError(
+                'Name must contain 20 characters or less.'
+            )
+        return last_name
+
     def clean_email2(self):
         email1 = self.cleaned_data.get("email1")
         email2 = self.cleaned_data.get("email2")
         if email1 and email2 and email1 != email2:
             raise forms.ValidationError(
-                'Emails do not match'
+                'Emails do not match.'
             )
         else:
             inuse = not User.objects.filter(email=self.cleaned_data["email2"])
             if not inuse:
                 raise forms.ValidationError(
-                    'Email is already in use'
+                    'Email is already in use.'
                 )
         return email2
