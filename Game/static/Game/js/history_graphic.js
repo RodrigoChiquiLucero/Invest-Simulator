d5aeff2c7ea7ca4fd0bd804842b511e58b107c7f
rusito23 <i.andruskiewitsch23@gmail.com>
2018-10-18 15:53:13 -0300

    asset-history - no voy a reinventar la rueda jaja le falta estilo no mas

--- Game/static/Game/js/history_graphic.js
+++ Game/static/Game/js/history_graphic.js
@@ -1,34 +1,69 @@
+const container = document.getElementById("myChart");
+const raw_data = container.getAttribute("data-js-vars").replace(/'/g, '"');
+const data = JSON.parse(raw_data || '{}');
 
-const s = function( sketch ) {
+let dates = data.map(function(item) {
+    return item.day;
+});
+let buys = data.map(function(item) {
+    return item.buy;
+});
+let sells = data.map(function(item) {
+    return item.sell;
+});
 
-  sketch.setup = function() {
-    let count = Object.keys(sketch.history).length;
-    let height = count * (50) + 140;
-    let width = count * (80);
-    sketch.createCanvas(width,height);
-    sketch.background(255);
-  };
-
-  sketch.draw = function() {
-      //set color
-      sketch.fill(0);
-      //draw lines
-      sketch.line(60, 20, 60, sketch.height - 20);
-      sketch.line(20, sketch.height - 60, sketch.width - 20, sketch.height - 60);
-
-      sketch.fill(0, 255, 0);
-      var start = 70;
-      var other = 330;
-      let price = 0;
-      //recorrer todos los datos de la history
-      $.each(sketch.history, function(i, item) {
-          sketch.text(item["day"], start, 360);
-          sketch.text(-price, 30, other);
-          start += 70;
-          other -= 50;
-          price -= 30;
-      });
-
-  };
-};
-
+let ctx = document.getElementById("myChart").getContext('2d');
+let myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: dates,
+        datasets: [{
+            label: 'Buy price',
+            data: buys,
+            backgroundColor: [
+                'rgba(  255,    99,     132,    0.2)',
+                'rgba(  54,     162,    235,    0.2)',
+                'rgba(  255,    206,    86,     0.2)',
+                'rgba(  75,     192,    192,    0.2)',
+                'rgba(  153,    102,    255,    0.2)',
+                'rgba(  255,    159,    64,     0.2)'
+            ],
+            borderColor: [
+                'rgba(  255,    99,     132,    1)',
+                'rgba(  54,     162,    235,    1)',
+                'rgba(  255,    206,    86,     1)',
+                'rgba(  75,     192,    192,    1)',
+                'rgba(  153,    102,    255,    1)',
+            ],
+            borderWidth: 1
+        },{
+            label: 'Sell price',
+            data: sells,
+            backgroundColor: [
+                'rgba(  132,  99,     255,    0.2)',
+                'rgba(  235,  162,    54,     0.2)',
+                'rgba(  86,   206,    255,    0.2)',
+                'rgba(  192,  192,    75,     0.2)',
+                'rgba(  255,  102,    153,    0.2)',
+                'rgba(  64,   159,    255,    0.2)'
+            ],
+            borderColor: [
+                'rgba(  132,  99,     255,    1)',
+                'rgba(  235,  162,    54,     1)',
+                'rgba(  86,   206,    255,    1)',
+                'rgba(  192,  192,    75,     1)',
+                'rgba(  255,  102,    153,    1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
