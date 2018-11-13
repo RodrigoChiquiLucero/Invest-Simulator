7647d8d09d4c47e35df568937e94b05e1e328b80
rusito23 <i.andruskiewitsch23@gmail.com>
2018-11-13 14:31:05 -0300

    hotfix - arreglos varios

--- InvestSimulator/settings.py
+++ InvestSimulator/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-API_URL = 'http://localhost/simulations/'
+API_URL = 'http://localhost:8000/simulations/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'InvestSimulator/media')
 MEDIA_URL = '/media/'
